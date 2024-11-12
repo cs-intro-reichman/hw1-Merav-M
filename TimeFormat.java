4aed5afa3e3e18f6a0d856afe7f07e78395b276e
Merav-M <merav.mauhnoom@post.runi.ac.il>
2024-11-13 00:01:12 +0200

    Time Format + Ascend

--- TimeFormat.java
+++ TimeFormat.java
@@ -6,12 +6,20 @@ public class TimeFormat {
 		// by the single command-line string argument args[0]. 
 		//   
 		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
+		// It concatenates the empty string "" with the leftmost hour-digit.
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+
+		String sTimeOfTheDay = "AM";
+		if (hours > 12)
+		{
+			hours = hours % 12;
+			sTimeOfTheDay = "PM";
+		}
+
+		System.out.println(hours + ":" + ((minutes < 10) ? ("0" + minutes) : minutes) + " " + sTimeOfTheDay);
 	}
 }
