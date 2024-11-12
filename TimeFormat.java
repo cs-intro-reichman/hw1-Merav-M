28486a9233d1290cdbdca6bc0157fe2438de4561
Merav-M <merav.mauhnoom@post.runi.ac.il>
2024-11-13 00:24:21 +0200

    Fix when input is 12 hours

--- TimeFormat.java
+++ TimeFormat.java
@@ -13,12 +13,8 @@ public class TimeFormat {
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
 
-		String sTimeOfTheDay = "AM";
-		if (hours > 12)
-		{
-			hours = hours % 12;
-			sTimeOfTheDay = "PM";
-		}
+		String sTimeOfTheDay = (hours < 12) ? "AM" : "PM";
+		hours = (hours == 12) ? 12 : hours % 12;
 
 		System.out.println(hours + ":" + ((minutes < 10) ? ("0" + minutes) : minutes) + " " + sTimeOfTheDay);
 	}
